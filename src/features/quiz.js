00c07326a008fbaf35f2c75281c7a9a9bab4ff4a
George Zhang <george.jx.zhang@gmail.com>
2020-09-19 19:46:57 -0600

    demo to everyone

--- src/features/quiz.js
+++ src/features/quiz.js
@@ -8,25 +8,25 @@ Survey
     .applyTheme( "bootstrap" );
 Survey.defaultBootstrapCss.navigationButton = "btn btn-green";
 
-export default function () { 
-    const model = new Survey.Model(test_json);
+export default function ( props ) {
+    const model = new Survey.Model( test_json );
 
-    const onValueChanged = (result) => {
-        console.log("value changed!");
+    const onValueChanged = ( result ) => {
+        console.log( "value changed!" );
     };
-    
-    const onComplete = (result) => {
-        console.log("Complete! " + result);
+
+    const onComplete = ( result ) => {
+        console.log( "Complete! " + result );
     };
 
     return (
-    <div className="container">
-        <h2>This is our test!</h2>
-        <Survey.Survey
-            model={model}
-            onComplete={onComplete}
-            onValueChanged={onValueChanged}
-          />
-    </div>
+        <div className="container">
+            <h2>This is our test!</h2>
+            <Survey.Survey
+                model={ model }
+                onComplete={ onComplete }
+                onValueChanged={ onValueChanged }
+            />
+        </div>
     );
 };
