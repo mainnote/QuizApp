b6cbdcb28e69f6eeccf5f6e52ede00db6aa9ce9c
ali2252usman <66345056+ali2252usman@users.noreply.github.com>
2020-09-24 20:05:50 -0600

    This is my code
    This is my code

--- src/features/menu.js
+++ src/features/menu.js
@@ -3,31 +3,63 @@ import { Link } from "react-router-dom";
 
 export default function ( props ) {
     return (
-        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <a className="navbar-brand" href="/">QuizApp</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/quiz">Quiz</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/results">Results</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/content">Test Content</Link>
-                    </li>
-                </ul>
-                <form className="form-inline mt-2 mt-md-0">
-                    <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
+        <html>
+<head>
+    <tile></tile>
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+        <div class="menu">
+            <ul>
+                <li class="logo"></li><img src="diamond.png" width="50" height="50"  ></img>
+                <li class="active">Navigation</li>
+                <li class="active">Home page</li>
+                <li class="active">Survey page: Questions and Answers</li>
+                <li class="active">Result page</li>
+                <li><a class="active">Login page</li>
+                <li><a class="active">Footer</li>
+                <li><a class="active">Privacy</li>
+                <li><a class="active">Terms of use</li>
+                
+            </ul>
+        </div>
+        <div class="banner">
+            <div class="app-text">
+                <h1>Check out <br>our survey.</h1>
+                <p>Here a brief description can  <br> go for the app and church.  </p>
+                <div class="play-btn">
+                    <div class="play-btn-inner"><i class="fa fa-play" aria-hidden="false"></i></div>
+                    <small><b>Learn More</b></small>
+                </div>
             </div>
-        </nav>
+            <div class="app-picture">
+                <img src="picture.png">
+            </div>
+        </div>
+        <div class="quick-links">
+            <ul>
+            <li><i class="fa fa-share-alt"></i><p>Share to</p></li>
+            <li><i class="fa fa-audio-description"></i><p>Recent news</p></li>
+            <li><i class="fa fa-cog"></i><p>Chinese church</p></li>
+            <li><i class="fa fa-btc"></i><p>Survey</p></li>
+            </ul>
+        </div>
+        <div class="social-icons">
+            <ul>
+                <li><a href="https://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="https://www.youtube.com/"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+            </ul>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
     );
 };
